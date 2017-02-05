62060fc52999da2ce9f25d467252fd6240b25bb7
7Tomus <tomuso.o@gmail.com>
2017-02-05 20:02:54 +0100

    implemented projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,27 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all.order('created_at DESC')
+  end
+
+  def new
+
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to @project
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
+
+end
