d11c23efdb00b20dab8754f2669a8a7e2f6bd114
7Tomus <tomuso.o@gmail.com>
2017-02-06 21:43:38 +0100

    project edit + delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,25 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(params[:project].permit(:title, :description))
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to root_path
+  end
+
   private
 
   def project_params
